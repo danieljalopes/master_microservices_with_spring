e1bd171b7776a506677f85a98735a77f2141e227
Daniel lopes <danieljalopes@xxx.xx>
2019-12-06 16:00:55 +0000

    Project Creation

---
+++ src/test/java/eu/danieljalopes/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.danieljalopes.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
