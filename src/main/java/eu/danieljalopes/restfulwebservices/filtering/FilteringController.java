0035721d09300825d7aea34d6fca98f7dffd20bf
danieljalopes <danieljalopes@xxx.xx>
2019-12-12 17:55:03 +0000

    Static filtering of a RESTful Service - ignore a field on the response

---
+++ src/main/java/eu/danieljalopes/restfulwebservices/filtering/FilteringController.java
@@ -0,0 +1,21 @@
+package eu.danieljalopes.restfulwebservices.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public SomeBean retrieveSomeBean() {
+		return new SomeBean("value 1", "value 2", "value 3");
+	}
+	
+	@GetMapping("/filtering-list")
+	public List<SomeBean> retrieveSomeBeanList() {
+		return  Arrays.asList(new SomeBean("value 1", "value 2", "value 3"),new SomeBean("value 12", "value 22", "value 32"));
+	}
+}
