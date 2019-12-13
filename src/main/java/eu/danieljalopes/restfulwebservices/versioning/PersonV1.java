2eedab2b06fe07691e410c3fdc2248f25f9c6f3a
danieljalopes <danieljalopes@xxx.xx>
2019-12-13 10:16:45 +0000

    Refactor PersonV1 to have only name as property

--- src/main/java/eu/danieljalopes/restfulwebservices/versioning/PersonV1.java
+++ src/main/java/eu/danieljalopes/restfulwebservices/versioning/PersonV1.java
@@ -1,9 +1,8 @@
 package eu.danieljalopes.restfulwebservices.versioning;
 
 public class PersonV1 {
-	private String firstName;
-	private String lastName;
-	
+	private String name;
+
 	
 	public PersonV1() {
 		super();
@@ -12,31 +11,21 @@ public class PersonV1 {
 
 	public PersonV1(String firstName, String lastName) {
 		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
+		this.name = firstName + " " + lastName;
+		
 	}
 
 
-	public String getFirstName() {
-		return firstName;
+	public String getName() {
+		return name;
 	}
 
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 
-	public String getLastName() {
-		return lastName;
-	}
-
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	
 	
 
 }
