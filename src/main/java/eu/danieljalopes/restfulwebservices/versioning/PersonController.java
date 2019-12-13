33e7cf00f9c161694d36eba91b07c0bd871fb25e
danieljalopes <danieljalopes@xxx.xx>
2019-12-13 10:28:43 +0000

    Add comments for better understanding of versioning on PersonController

--- src/main/java/eu/danieljalopes/restfulwebservices/versioning/PersonController.java
+++ src/main/java/eu/danieljalopes/restfulwebservices/versioning/PersonController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
+	/*------------------------------------------------------
+	 * 
+	 * Using URI to return a different version of a Person
+	 * 
+	 -----------------------------------------------------*/
+	
 	@GetMapping("v1/person")
 	public PersonV1 getPersonV1() {
 		return new PersonV1("Bob", "Marley");
@@ -16,23 +22,39 @@ public class PersonController {
 		return new PersonV2("Bob", "Marley");
 	}
 	
-	@GetMapping(value="/person", params="version=1")
+	
+	/*------------------------------------------------------------
+	 * 
+	 * Using URI params to return a different version of a Person
+	 * 
+	 ------------------------------------------------------------*/
+	
+	@GetMapping(value="/person/params", params="version=1")
 	public PersonV1 getPersonV1Param() {
 		return new PersonV1("Bob", "Marley");
 	}
 	
-	@GetMapping(value="/person", params="version=2")
+	@GetMapping(value="/person/params", params="version=2")
 	public PersonV2 getPersonV2Param() {
 		return new PersonV2("Bob", "Marley");
 	}
 	
-	@GetMapping(value="/person", headers="X-API-VERSION=1")
+	/*-------------------------------------------------------------
+	 * 
+	 * Using the headers to return a different version of a Person
+	 * 
+	 ------------------------------------------------------------*/
+	
+	@GetMapping(value="/person/headers", headers="X-API-VERSION=1")
 	public PersonV1 getPersonV1Header() {
 		return new PersonV1("Bob", "Marley");
 	}
 	
-	@GetMapping(value="/person", headers="X-API-VERSION=2")
+	@GetMapping(value="/person/headers", headers="X-API-VERSION=2")
 	public PersonV2 getPersonV2Header() {
 		return new PersonV2("Bob", "Marley");
 	}
+	
+	
+	
 }
