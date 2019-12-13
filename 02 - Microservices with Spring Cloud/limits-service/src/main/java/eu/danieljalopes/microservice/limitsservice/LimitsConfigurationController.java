9f17de9c3caff21e691ffd6f9b442ed1d9e78d86
danieljalopes <danieljalopes@xxx.xx>
2019-12-13 15:55:58 +0000

    Limits Configuration Micro-service
    Properties are retrieved from the application.properties file

--- 02 - Microservices with Spring Cloud/limits-service/src/main/java/eu/danieljalopes/microservice/limitsservice/LimitsConfigurationController.java
+++ 02 - Microservices with Spring Cloud/limits-service/src/main/java/eu/danieljalopes/microservice/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package eu.danieljalopes.microservice.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import eu.danieljalopes.microservice.limitsservice.bean.LimitsConfiguration;
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration configuration;
+
 	@GetMapping("/limits")
 	public LimitsConfiguration retrieveLimitsConfigurations() {
-		return new LimitsConfiguration(1000, 1);
+		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
