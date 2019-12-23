b9d30e47e9f11ad74edd3c9a302834f32c94ba0d
danieljalopes <danieljalopes@xxx.xx>
2019-12-23 17:57:40 +0000

    Currency Exchange Service
    Setting Load Balancing with Ribbon

--- 02 - Microservices with Spring Cloud/currrency-conversion-service/src/main/java/eu/danieljalopes/microservice/currrencyconversionservice/CurrencyExchangeProxyService.java
+++ 02 - Microservices with Spring Cloud/currrency-conversion-service/src/main/java/eu/danieljalopes/microservice/currrencyconversionservice/CurrencyExchangeProxyService.java
@@ -1,11 +1,14 @@
 package eu.danieljalopes.microservice.currrencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxyService {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
