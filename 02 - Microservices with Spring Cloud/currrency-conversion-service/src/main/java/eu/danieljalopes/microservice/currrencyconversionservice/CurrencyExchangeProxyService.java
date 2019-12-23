6e6d4cf1caf9c2998762f6a8123a2009e607fce1
danieljalopes <danieljalopes@xxx.xx>
2019-12-23 16:26:35 +0000

    Currency Conversion Service
    Using Feign to consume REST services.
    Changed Spring boot version to 2.2.RELEASE on
    currency-conversion-service and currency-exchange-service projects.
    Changed Spring Cloud version to Hoxton.SR1 on
    currency-conversion-service and currency-exchange-service projects.

---
+++ 02 - Microservices with Spring Cloud/currrency-conversion-service/src/main/java/eu/danieljalopes/microservice/currrencyconversionservice/CurrencyExchangeProxyService.java
@@ -0,0 +1,14 @@
+package eu.danieljalopes.microservice.currrencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
+public interface CurrencyExchangeProxyService {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
--- 02 - Microservices with Spring Cloud/currrency-conversion-service/src/main/java/eu/danieljalopes/microservice/currrencyconversionservice/CurrrencyConversionServiceApplication.java
+++ 02 - Microservices with Spring Cloud/currrency-conversion-service/src/main/java/eu/danieljalopes/microservice/currrencyconversionservice/CurrrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package eu.danieljalopes.microservice.currrencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("eu.danieljalopes.microservice.currrencyconversionservice")
 public class CurrrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
