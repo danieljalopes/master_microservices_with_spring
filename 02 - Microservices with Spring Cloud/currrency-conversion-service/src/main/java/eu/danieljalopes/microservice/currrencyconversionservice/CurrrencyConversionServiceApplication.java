c18dbc8a7c74581771055920b8a54c5132ebc4a1
danieljalopes <danieljalopes@xxx.xx>
2019-12-27 10:21:28 +0000

    Eureka Naming Server
    Connect Currency Conversion Service to Eureka Naming Server

--- 02 - Microservices with Spring Cloud/currrency-conversion-service/src/main/java/eu/danieljalopes/microservice/currrencyconversionservice/CurrrencyConversionServiceApplication.java
+++ 02 - Microservices with Spring Cloud/currrency-conversion-service/src/main/java/eu/danieljalopes/microservice/currrencyconversionservice/CurrrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package eu.danieljalopes.microservice.currrencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("eu.danieljalopes.microservice.currrencyconversionservice")
+@EnableDiscoveryClient
 public class CurrrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
