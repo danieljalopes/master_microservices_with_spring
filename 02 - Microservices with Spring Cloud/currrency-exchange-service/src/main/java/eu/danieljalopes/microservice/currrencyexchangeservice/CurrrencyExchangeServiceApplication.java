652e4c84517758330a569108f070145d923dde8c
danieljalopes <danieljalopes@xxx.xx>
2019-12-27 10:33:43 +0000

    Eureka Naming Server
    Connect Currency Exchange Service to Eureka Naming Service

--- 02 - Microservices with Spring Cloud/currrency-exchange-service/src/main/java/eu/danieljalopes/microservice/currrencyexchangeservice/CurrrencyExchangeServiceApplication.java
+++ 02 - Microservices with Spring Cloud/currrency-exchange-service/src/main/java/eu/danieljalopes/microservice/currrencyexchangeservice/CurrrencyExchangeServiceApplication.java
@@ -2,8 +2,10 @@ package eu.danieljalopes.microservice.currrencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
